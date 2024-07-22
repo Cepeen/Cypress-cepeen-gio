6a939aff5d6cf2b5260459a96b4eb2d303e1eeed
Tomek Tomasik <32199115+Cepeen@users.noreply.github.com>
2024-07-22 18:09:59 +0200

    search

--- cypress/e2e/entries.cy.js
+++ cypress/e2e/entries.cy.js
@@ -2,21 +2,32 @@
 
 
 //search tests
+describe('Blog Search', () => {
+  it('should search for each blog title found in blog-card elements', () => {
+      cy.visit('https://www.tomektomasik.pl/blog')
+      cy.on('uncaught:exception', (err, runnable) => {
+          return false 
+      })
 
-describe('Enter search query', () => {
-    it("Entering querry", () => {
-        cy.visit('https://www.tomektomasik.pl/blog')
-        cy.on('uncaught:exception', (err, runnable) => {
-          return false
-        })
-        cy.get("#search-input").type("mighty adventures!")
-      })})
-
-      describe ('Enter search query', () => {
-        it("Entering querry", () => {
-          cy.visit('https://www.tomektomasik.pl/blog')
-          cy.on('uncaught:exception', (err, runnable) => {
-            return false
+     
+      cy.get('.title.is-size-4-touch').then($titles => {
+          const titles = []
+          $titles.each((index, $title) => {
+              const titleText = $title.innerText
+              if (titleText) {
+                  titles.push(titleText.trim()) 
+              }
           })
-          cy.get("#search-input").type("How Dare You!")
-        })})
+
+          // Iterate over each title and perform the search
+          titles.forEach(title => {
+              cy.visit('https://www.tomektomasik.pl/blog')
+              cy.on('uncaught:exception', (err, runnable) => {
+                  return false 
+              })
+              cy.get('#search-input').type(`${title}{enter}`)
+              cy.contains(title).should('be.visible')
+          })
+      })
+  })
+})
--- cypress/e2e/initial.cy.js
+++ cypress/e2e/initial.cy.js
@@ -1,27 +1,23 @@
 /// <reference types="cypress" />
 
-
-
-
 describe('Homepage', () => {
-    it('should load successfully', () => {
+  it('should load successfully', () => {
       cy.visit("https://www.tomektomasik.pl")
       cy.contains('Tomek Tomasik')
-
-    })
   })
+})
 
-  
-  //basic navigation tests
-  describe('Navigation', () => {
-    beforeEach(() => {
+// Basic navigation tests
+describe('Navigation', () => {
+  beforeEach(() => {
       cy.visit('https://www.tomektomasik.pl')
       cy.on('uncaught:exception', (err, runnable) => {
-        return false 
+          return false
       })
-    })
-  
-    const links = [
+  })
+
+  const links = [
+      // Navbar
       { selector: 'a[href="/#about"]', urlPart: '/#about', text: 'I am a jelly enthusiast.' },
       { selector: 'a[href="/#contact"]', urlPart: '/#contact', text: 'Contact Us' },
       { selector: 'a[href="/3d-models"]', urlPart: '/3d-models', text: '3D Models' },
@@ -29,16 +25,50 @@ describe('Homepage', () => {
       { selector: 'a[href="/music"]', urlPart: '/music', text: 'Music' },
       { selector: 'a[href="/apps"]', urlPart: '/apps', text: 'APPS' },
       { selector: 'a[href="/blog"]', urlPart: '/blog', text: 'blog' },
-    ]
-  
-    links.forEach(link => {
-      it(`should navigate to the ${link.urlPart} page`, () => {
-        cy.get(link.selector).each(($el) => {
-          cy.wrap($el).click({force: true})
-          cy.url().should('include', link.urlPart)
-          cy.go('back')
-        })
-      })
-    })
-  })
+      // Footer
+      { selector: 'a[href="/privacy-policy"]', urlPart: '/privacy-policy', text: 'privacy-policy' },
+  ]
 
+  links.forEach(link => {
+      it(`should navigate to the ${link.urlPart} page`, () => {
+          cy.get(link.selector).then($el => {
+              if ($el.length) {
+                  cy.wrap($el).click({ force: true })
+                  cy.url().should('include', link.urlPart)
+                  cy.go('back')
+              } else {
+                  cy.log(`Element not found: ${link.selector}`)
+              }
+          })
+      })
+  })
+})
+
+describe('Blog Navigation', () => {
+  it('should navigate to all links inside blog-card elements and verify navigation', () => {
+
+      cy.visit('https://www.tomektomasik.pl/blog')
+      cy.on('uncaught:exception', (err, runnable) => {
+       
+          return false
+      })
+
+    
+      cy.get('#blog-card a').then($links => {
+          const links = []
+          $links.each((index, $link) => {
+              const href = $link.getAttribute('href')
+              if (href) {
+                  links.push(href)
+              }
+          })
+
+  
+          links.forEach(link => {
+              cy.visit(link)
+              cy.url().should('include', link)
+              cy.go('back')
+          })
+      })
+  })
+})
