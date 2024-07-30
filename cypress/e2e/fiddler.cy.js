4b7b034e332cecd109d3d753fc59e5ed5f908a79
Tomek Tomasik <32199115+Cepeen@users.noreply.github.com>
2024-07-30 14:45:49 +0200

    navigation tests

--- cypress/e2e/fiddler.cy.js
+++ cypress/e2e/fiddler.cy.js
@@ -13,14 +13,13 @@ describe('Last.fm Fiddler', () => {
         cy.get('#tracksPerPage').select('10')
         cy.get('#recentbutton').click()
 
-    
         cy.wait(2000)
 
         cy.get('#dataContainer').should('be.visible')
         cy.get('#dataContainer').children().should('have.length', 10)
     })
 
-    it('should fetch and display selected number of tracks', () => {
+    it('should fetch and display selected number of tracks (20)', () => {
         cy.get('#username').type('do_not_exist')
         cy.get('#tracksPerPage').select('20')
         cy.get('#recentbutton').click()
@@ -29,5 +28,80 @@ describe('Last.fm Fiddler', () => {
 
         cy.get('#dataContainer').should('be.visible')
         cy.get('#dataContainer').children().should('have.length', 20)
+    })
+
+    it('should fetch and display selected number of tracks (50)', () => {
+        cy.get('#username').type('do_not_exist')
+        cy.get('#tracksPerPage').select('50')
+        cy.get('#recentbutton').click()
+
+        cy.wait(2000)
+
+        cy.get('#dataContainer').should('be.visible')
+        cy.get('#dataContainer').children().should('have.length', 50)
+    })
+
+    it('should open the sidebar with lyrics when the lyrics button is clicked', () => {
+        cy.get('#username').type('do_not_exist')
+        cy.get('#tracksPerPage').select('10')
+        cy.get('#recentbutton').click()
+
+        // Wait for the data to load
+        cy.wait(2000)
+
+        // Verify that data is loaded
+        cy.get('#dataContainer').should('be.visible')
+        cy.get('#dataContainer').children().should('have.length', 10)
+
+        // Verify that the lyrics button becomes visible
+        cy.get('#LyricsButton').should('be.visible')
+        
+        // Check the initial state of the sidebar
+        cy.get('#searchResultsContainer').should('not.be.visible')
+
+        // Click the lyrics button
+        cy.get('#LyricsButton').click()
+
+        // Scroll into view and verify that the sidebar becomes visible and contains text
+        cy.get('#searchResultsContainer').scrollIntoView().should('be.visible')
+        cy.get('#searchResultsContainer').should('not.be.empty')
+    })
+
+    it('should navigate through pagination correctly', () => {
+        cy.get('#username').type('do_not_exist')
+        cy.get('#tracksPerPage').select('10')
+        cy.get('#recentbutton').click()
+
+        // Wait for the data to load
+        cy.wait(2000)
+
+        // Verify initial state of pagination
+        cy.get('#currentPage').should('contain', '1')
+        cy.get('#totalPages').invoke('text').then((totalPages) => {
+            const totalPagesInt = parseInt(totalPages, 10)
+            expect(totalPagesInt).to.be.greaterThan(1)
+
+            // Navigate to the next page
+            cy.get('#nextPage').click()
+            cy.wait(1000)
+            cy.get('#currentPage').should('contain', '2')
+
+            // Navigate back to the previous page
+            cy.get('#prevPage').click()
+            cy.wait(1000)
+            cy.get('#currentPage').should('contain', '1')
+
+            // Navigate to the last page
+            cy.get('#pageInput').clear().type(totalPagesInt)
+            cy.get('#goToPage').click()
+            cy.wait(1000)
+            cy.get('#currentPage').should('contain', totalPagesInt)
+
+            // Navigate back to the first page
+            cy.get('#pageInput').clear().type('1')
+            cy.get('#goToPage').click()
+            cy.wait(1000)
+            cy.get('#currentPage').should('contain', '1')
+        })
     })
 })
--- cypress/e2e/initial.cy.js
+++ cypress/e2e/initial.cy.js
@@ -1,77 +1,113 @@
 /// <reference types="cypress" />
 
 describe('Homepage', () => {
-  it('should load successfully', () => {
-      cy.visit("https://www.tomektomasik.pl")
-      cy.contains('Tomek Tomasik')
+    it('should load successfully', () => {
+        cy.visit("https://www.tomektomasik.pl")
+        cy.contains('Tomek Tomasik')
+    })
   })
-})
-
-// Basic navigation tests
-describe('Navigation', () => {
-  beforeEach(() => {
-      cy.visit('https://www.tomektomasik.pl')
-      cy.on('uncaught:exception', (err, runnable) => {
-          return false
-      })
-  })
-
-  const links = [
-      // Navbar
-      { selector: 'a[href="/#about"]', urlPart: '/#about', text: 'I am a jelly enthusiast.' },
-      { selector: 'a[href="/#contact"]', urlPart: '/#contact', text: 'Contact Us' },
-      { selector: 'a[href="/3d-models"]', urlPart: '/3d-models', text: '3D Models' },
-      { selector: 'a[href="/artwork"]', urlPart: '/artwork', text: 'Gallery' },
-      { selector: 'a[href="/music"]', urlPart: '/music', text: 'Music' },
-      { selector: 'a[href="/apps"]', urlPart: '/apps', text: 'APPS' },
-      { selector: 'a[href="/blog"]', urlPart: '/blog', text: 'blog' },
-      // Footer
-      { selector: 'a[href="/privacy-policy"]', urlPart: '/privacy-policy', text: 'privacy-policy' },
-  ]
-
-  links.forEach(link => {
+  
+  // Basic navigation tests
+  describe('Navigation', () => {
+    beforeEach(() => {
+        cy.visit('https://www.tomektomasik.pl')
+        cy.on('uncaught:exception', (err, runnable) => {
+            return false
+        })
+    })
+  
+    const links = [
+        // Navbar
+        { selector: 'a[href="/#about"]', urlPart: '/#about', text: 'I am a jelly enthusiast.' },
+        { selector: 'a[href="/#contact"]', urlPart: '/#contact', text: 'Contact Us' },
+        { selector: 'a[href="/3d-models"]', urlPart: '/3d-models', text: '3D Models' },
+        { selector: 'a[href="/artwork"]', urlPart: '/artwork', text: 'Gallery' },
+        { selector: 'a[href="/music"]', urlPart: '/music', text: 'Music' },
+        { selector: 'a[href="/apps"]', urlPart: '/apps', text: 'APPS' },
+        { selector: 'a[href="/blog"]', urlPart: '/blog', text: 'blog' },
+        // Footer
+        { selector: 'a[href="/privacy-policy"]', urlPart: '/privacy-policy', text: 'privacy-policy' },
+    ]
+  
+    links.forEach(link => {
       it(`should navigate to the ${link.urlPart} page`, () => {
-          cy.get(link.selector).then($el => {
-              if ($el.length) {
-                  cy.wrap($el).click({ force: true })
-                  cy.url().should('include', link.urlPart)
-                  cy.go('back')
-              } else {
-                  cy.log(`Element not found: ${link.selector}`)
-              }
-          })
+        cy.get(link.selector).first().as('targetLink')
+        
+        cy.get('@targetLink').then($el => {
+          if ($el.length) {
+            const isInDropdown = $el.closest('.navbar-item.has-dropdown').length > 0
+  
+            if (isInDropdown) {
+              cy.get('@targetLink').closest('.navbar-item.has-dropdown').click()
+   
+              cy.wait(500)
+            }
+  
+            cy.get('@targetLink').should('be.visible').then($visibleEl => {
+   
+              cy.wrap($visibleEl).invoke('attr', 'target', '_self')
+                .invoke('removeAttr', 'data-turbolinks')
+                .click({ force: true })
+            })
+  
+
+            cy.url().should('include', link.urlPart)
+            
+
+            cy.go('back')
+          } else {
+            cy.log(`Element not found: ${link.selector}`)
+          }
+        })
       })
+    })
   })
-})
-
-describe('Blog Navigation', () => {
-  it('should navigate to all links inside blog-card elements and verify navigation', () => {
-
-      cy.visit('https://www.tomektomasik.pl/blog')
-      cy.on('uncaught:exception', (err, runnable) => {
-       
-          return false
-      })
-
+  
+  describe('Blog Navigation', () => {
+    it('should navigate to all links inside blog-card elements and verify navigation', () => {
+  
+        cy.visit('https://www.tomektomasik.pl/blog')
+        cy.on('uncaught:exception', (err, runnable) => {
+         
+            return false
+        })
+  
+      
+        cy.get('#blog-card a').then($links => {
+            const links = []
+            $links.each((index, $link) => {
+                const href = $link.getAttribute('href')
+                if (href) {
+                    links.push(href)
+                }
+            })
+  
     
-      cy.get('#blog-card a').then($links => {
-          const links = []
-          $links.each((index, $link) => {
-              const href = $link.getAttribute('href')
-              if (href) {
-                  links.push(href)
-              }
-          })
+            links.forEach(link => {
+                cy.visit(link)
+                cy.url().should('include', link)
+                cy.go('back')
+            })
+        })
+    })
+  })
+  
+  
+  
+  describe('Performance', () => {
+    it('Should load the homepage within acceptable time', () => {
+      cy.visit('https://www.tomektomasik.pl', {
+        onBeforeLoad: (win) => {
+          win.performance.mark('start-loading')
+        },
+      }).then((win) => {
+        win.performance.mark('end-loading')
+        win.performance.measure('page-load-time', 'start-loading', 'end-loading')
+        const measure = win.performance.getEntriesByName('page-load-time')[0]
+        expect(measure.duration).to.be.below(3000)
+      })
+    })
+  })
+
 
   
-          links.forEach(link => {
-              cy.visit(link)
-              cy.url().should('include', link)
-              cy.go('back')
-          })
-      })
-  })
-})
-
-
-
